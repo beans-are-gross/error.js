f90bbeb7fe15de467e4e94cb880197fa1bff9e8c
beans-are-gross <94317146+beans-are-gross@users.noreply.github.com>
2023-11-21 15:59:34 -0500

    Add files via upload

---
+++ error.js
@@ -0,0 +1,16 @@
+window.addEventListener("error", (ErrorEvent) => {
+  const errorJs = document.getElementById("error-js");
+
+  errorJs.innerHTML =
+    '<div id="error-container"> <div id="error"> <img src="error.svg" /> <h1>Error</h1> <p>Your printer has crashed.</p> <div id="error-info"> <h2>Error Info:</h2> <h3>Message:</h3> <p>' +
+    ErrorEvent.message +
+    "</p> <h3>Location:</h3> <p>" +
+    ErrorEvent.filename +
+    "</p> <h3>Line #:</h3> <p>" +
+    ErrorEvent.lineno +
+    "</p> <h3>Col #:</h3> <p>" +
+    ErrorEvent.colno +
+    "</p> <h3>Object</h3> <p>" +
+    ErrorEvent.error +
+    "</p> </div> </div> </div>";
+});
