d09207351b04d2ccea3b1e10e4853522ceb2156f
beans-are-gross <94317146+beans-are-gross@users.noreply.github.com>
2023-11-24 20:32:18 -0500

    Update error.js

--- error.js
+++ error.js
@@ -1,16 +1,16 @@
-window.addEventListener("error", (ErrorEvent) => {
+window.addEventListener("error", (error) => {
   const errorJs = document.getElementById("error-js");
 
   errorJs.innerHTML =
     '<div id="error-container"> <div id="error"> <img src="https://raw.githubusercontent.com/beans-are-gross/error.js/main/error.svg" /> <h1>Error</h1> <p>Your code has crashed.</p> <div id="error-info"> <h2>Error Info:</h2> <h3>Message:</h3> <p>' +
-    ErrorEvent.message +
+    error.message +
     "</p> <h3>Location:</h3> <p>" +
-    ErrorEvent.filename +
+    error.filename +
     "</p> <h3>Line #:</h3> <p>" +
-    ErrorEvent.lineno +
+    error.lineno +
     "</p> <h3>Col #:</h3> <p>" +
-    ErrorEvent.colno +
+    error.colno +
     "</p> <h3>Object</h3> <p>" +
-    ErrorEvent.error +
+    error.error +
     "</p> </div> </div> </div>";
 });
